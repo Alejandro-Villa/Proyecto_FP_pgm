44af87b5a33ca07c19cfb7936fe4fed88e037762
Alejandro-Villa <43407560+Alejandro-Villa@users.noreply.github.com>
2018-11-09 19:14:20 +0100

    Update funciones_utiles.cpp

--- funciones_utiles.cpp
+++ funciones_utiles.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+#include<cstring>
+
 using namespace std;
 // Este es el tipo de datos que vamos a usar todos
  struct imagen{
@@ -6,10 +9,32 @@ using namespace std;
 
    int alto;
 
-   int imagen[650][650];
+   int matriz[650][650];
 
   };
+//Función para leer los datos del ficchero .pmg
+
+imagen Leer_datos( ) {
+  imagen ima_input;
+  int valor_blanco, alto;
+  string P2;
+  
+ 
+  cin >> P2;
+ 
+  cin >> imagen.ancho;
+  cin >> imagen.alto;
+ 
+  cin >> valor_blanco;
+  for ( int j = 0; j < imagen.alto; j++ )
+   for ( int i = 0; i < imagen.ancho;ij++){
+    cin >> imagen.matriz[i][j];
+   }
+ 
+}
 
 int main(){
   
+ 
+ 
 }
