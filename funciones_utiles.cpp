3bc3adf88116ccc8fbef85d2809286304e8c7172
Alejandro-Villa <43407560+Alejandro-Villa@users.noreply.github.com>
2018-11-09 18:41:57 +0100

    Update funciones_utiles.cpp

--- funciones_utiles.cpp
+++ funciones_utiles.cpp
@@ -1,7 +1,15 @@
 using namespace std;
+// Este es el tipo de datos que vamos a usar todos
+ struct imagen{
+
+   int ancho;
+
+   int alto;
+
+   int imagen[650][650];
+
+  };
 
 int main(){
-  
-  
   
 }
