0849ad5d56a015f7040b673a4b5e4540bb9df6d9
Alejandro-Villa <43407560+Alejandro-Villa@users.noreply.github.com>
2018-11-26 12:27:00 +0100

    Update funciones_utiles.cpp

--- funciones_utiles.cpp
+++ funciones_utiles.cpp
@@ -1,29 +1,25 @@
 using namespace std;
 // Este es el tipo de datos que vamos a usar todos
- struct imagen{
+struct imagen{
 
-   int ancho;
+	int ancho;
 
-   int alto;
+	int alto;
 
-   int imagen[650][650];
+	int imagen[650][650];
 
-  };
+};
 
 //Función de Salida de datos.
 void SalidaDatos(imagen fichero){
-	
-	cout << "P2\n";
+	cout << "P2" << "\n";
 	cout << fichero.ancho << " " << fichero.alto << "\n";
-	cout << "255\n";
-	
-	for(int i = 0 ; i < fichero.alto ; i++){
-		for(int j = 0 ; j < fichero.ancho ; j++){
-			cout << fichero.imagen[i][j] << " ";
-			if(j == fichero.ancho -1)
-				cout << "\n";
-		}
-	}
+	cout << "255" << endl;
+
+	for(int j = 0; j < fichero.alto; j++ ){
+		for(int i = 0; i < fichero.ancho; i++)
+			cout << fichero.imagen[i][j] << "\n";
+    	}
 }
 
 int main(){
